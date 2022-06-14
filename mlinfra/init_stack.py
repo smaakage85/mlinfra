6dd9e4b8a89f4a358e67fd912d449df3c34c9c5c
smaakage85 <lars_kjeldgaard@hotmail.com>
2022-06-14 13:14:19 +0200

    first commit

---
+++ mlinfra/init_stack.py
@@ -0,0 +1,25 @@
+import aws_cdk as cdk
+from aws_cdk import (
+    aws_ecr as ecr,
+    aws_s3 as s3,
+    aws_iam as iam,
+    aws_ec2 as ec2,
+)
+            
+class InitStack(cdk.Stack):
+
+    def __init__(self, scope: cdk.App, construct_id: str, **kwargs) -> None:
+        super().__init__(scope, construct_id, **kwargs)
+
+        artifact_bucket = s3.Bucket(
+            self,
+            "ArtifactBucket",
+            block_public_access=s3.BlockPublicAccess.BLOCK_ALL,
+            bucket_name="mlinfra-artifacts",
+        )
+
+        fireworks_repository = ecr.Repository(
+            self, 
+            "ImageRepository", 
+            repository_name="mlinfra",
+        )
